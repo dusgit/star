0b90fb465bb94ba4c40d8885d63d6ef6a16ff59b
dustin <dus_2005@163.com>
2018-07-03 16:28:36 +0800

    add

--- routes/star.js
+++ routes/star.js
@@ -6,5 +6,9 @@ var star = require('../server/star');
 router.get('/', function(req, res, next) {
 	res.render('index', {title:'ejs'});
 });
+router.get('/news/:id', function(req, res, next) {
+	var id = req.params.id;
+	res.render('index', {title:'ejs'});
+});
 
 module.exports = router;
